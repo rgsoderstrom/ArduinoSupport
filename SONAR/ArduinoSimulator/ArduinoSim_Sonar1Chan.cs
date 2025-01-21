85bef6ab9635126674041810dbddd6641950e53c
Randy Soderstrom <rgsoderstrom@gmail.com>
2025-01-21 07:30:37 -0800

    Add noise level and some comments

--- SONAR/ArduinoSimulator/ArduinoSim_Sonar1Chan.cs
+++ SONAR/ArduinoSimulator/ArduinoSim_Sonar1Chan.cs
@@ -133,11 +133,12 @@ namespace ArduinoSimulator
 
         static Random random = new Random ();
 
-        double Range = 5; // feet
+        readonly double Range = 5; // feet
 
-        int    Ramp = 20;
-        double ampl = 200;
-        int    DC = 512;
+        readonly int    Ramp  = 20;
+        readonly double ampl  = 200;
+        readonly double noise = 10;
+        readonly int    DC    = 512;
 
         private void PingMessageHandler (byte [] msgBytes)
         {
@@ -152,41 +153,46 @@ namespace ArduinoSimulator
 
             double time = 0;
 
+            // before the target return begins
             for (int i=0; i<LeadingZero; i++, time+=1/SampleRate)
             {
-                double withNoiseAndDC = random.NextDouble () + DC;
+                double withNoiseAndDC = noise * random.NextDouble () + DC;
                 Samples.Add (Math.Truncate (withNoiseAndDC));
             }
 
+            // returns from time the xmit waveform is ramping up
             for (int i=0; i<Ramp; i++, time+=1/SampleRate)
             {
                 double win = (double) i / Ramp;
                 double s = win * ampl * Math.Sin (2 * Math.PI * PingFrequency * time);
-                double withNoiseAndDC = random.NextDouble () + DC + s;
+                double withNoiseAndDC = noise * random.NextDouble () + DC + s;
                 Samples.Add (Math.Truncate (withNoiseAndDC));
             }            
             
+            // from time at max level
             for (int i=0; i<PingSamples; i++, time+=1/SampleRate)
             {
                 double win = 1;
                 double s = win * ampl * Math.Sin (2 * Math.PI * PingFrequency * time);
-                double withNoiseAndDC = random.NextDouble () + DC + s;
+                double withNoiseAndDC = noise * random.NextDouble () + DC + s;
                 Samples.Add (Math.Truncate (withNoiseAndDC));
             }            
             
+            // while ramping down
             for (int i=0; i<Ramp; i++, time+=1/SampleRate)
             {
                 double win = 1 - (double) i / Ramp;
                 double s = win * ampl * Math.Sin (2 * Math.PI * PingFrequency * time);
-                double withNoiseAndDC = random.NextDouble () + DC + s;
+                double withNoiseAndDC = noise * random.NextDouble () + DC + s;
                 Samples.Add (Math.Truncate (withNoiseAndDC));
             }            
 
+            // after target return ends
             int rem = BatchSize - Samples.Count;
             
             for (int i=0; i<rem; i++, time+=1/SampleRate)
             {
-                double withNoiseAndDC = random.NextDouble () + DC;
+                double withNoiseAndDC = noise * random.NextDouble () + DC;
                 Samples.Add (Math.Truncate (withNoiseAndDC));
             }
 
